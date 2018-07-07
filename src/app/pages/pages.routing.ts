fe9ea174ab0ab75728ec3a9d0fd581df73a01916
yannick <nic.kabs01@gmail.com>
2018-07-07 10:57:31 +0200

    routing and module fix

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { InternationalVisitorsProgramComponent } from './international-visitors-program/international-visitors-program.component';
-import { PartnershipComponent } from './partnership/partnership.component';
-import { PropheticSchoolMinistryComponent } from './prophetic-school-ministry/prophetic-school-ministry.component';
-import { SheperdBushiriFoundationComponent } from './sheperd-bushiri-foundation/sheperd-bushiri-foundation.component';
-import { TelevisionMinistryComponent } from './television-ministry/television-ministry.component';
-import { ConferencesandseminarsComponent } from './conferencesandseminars/conferencesandseminars.component';
+// import { AboutComponent } from './about/about.component';
+// import { ContactComponent } from './contact/contact.component';
+// import { InternationalVisitorsProgramComponent } from './international-visitors-program/international-visitors-program.component';
+// import { PartnershipComponent } from './partnership/partnership.component';
+// import { PropheticSchoolMinistryComponent } from './prophetic-school-ministry/prophetic-school-ministry.component';
+// import { SheperdBushiriFoundationComponent } from './sheperd-bushiri-foundation/sheperd-bushiri-foundation.component';
+// import { TelevisionMinistryComponent } from './television-ministry/television-ministry.component';
+// import { ConferencesandseminarsComponent } from './conferencesandseminars/conferencesandseminars.component';
 
 const routes: Routes = [
   {
@@ -18,14 +18,14 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'prefix' },
       { path: 'home', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: 'international', component: InternationalVisitorsProgramComponent },
-      { path: 'partnership', component: PartnershipComponent },
-      { path: 'prophetic-school', component: PropheticSchoolMinistryComponent },
-      { path: 'sbfoundation', component: SheperdBushiriFoundationComponent },
-      { path: 'tvministry', component: TelevisionMinistryComponent },
-      { path: 'conferences', component: ConferencesandseminarsComponent }
+      // { path: 'about', component: AboutComponent },
+      // { path: 'contact', component: ContactComponent },
+      // { path: 'international', component: InternationalVisitorsProgramComponent },
+      // { path: 'partnership', component: PartnershipComponent },
+      // { path: 'prophetic-school', component: PropheticSchoolMinistryComponent },
+      // { path: 'sbfoundation', component: SheperdBushiriFoundationComponent },
+      // { path: 'tvministry', component: TelevisionMinistryComponent },
+      // { path: 'conferences', component: ConferencesandseminarsComponent }
     ]
   }
 ];
