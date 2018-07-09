507e35fa0634348121f617a1b7bee28697b3bc84
Enoch Abassey <eabassey@yahoo.com>
2018-07-09 20:08:30 +0200

    drop down

--- src/app/user/create-event/create-event.component.ts
+++ src/app/user/create-event/create-event.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { filter, tap, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-event',
@@ -9,13 +12,28 @@ import { Router } from '@angular/router';
 })
 export class CreateEventComponent implements OnInit {
   createEventForm: FormGroup;
-  currentEvents = ['Holy Ghost Conference', 'A touch from Above'];
-  constructor(private router: Router, private fb: FormBuilder) {}
+  currentEvents: any;
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit() {
+    this.db
+      .collection('/availableEvents')
+      .valueChanges()
+      .pipe(
+        takeWhile(ev => !!ev),
+        tap(console.log)
+      ).subscribe(events => this.currentEvents = events);
     this.createEventForm = this.fb.group({
       selectedEvent: ['']
     });
+  }
+
+  selectEvent(event) {
+    console.log(`Selected: ${JSON.stringify(event)}`);
   }
 
   createEvent() {
