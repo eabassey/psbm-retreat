922690d874a0ef266e7e1fbd37cc5a75cc23497c
Enoch Abassey <eabassey@yahoo.com>
2018-07-10 17:23:44 +0200

    production firebase keys; removing unused component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBdM-9G3pVMHB4fwi4SLvuKHMcZQA0GdLw',
+    authDomain: 'bspm-auth.firebaseapp.com',
+    databaseURL: 'https://bspm-auth.firebaseio.com',
+    projectId: 'bspm-auth',
+    storageBucket: 'bspm-auth.appspot.com',
+    messagingSenderId: '407062452144'
+  }
 };
