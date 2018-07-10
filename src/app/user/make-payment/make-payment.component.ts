419894a2a55b4d21fe693a422dbe23f4e8f64018
yannick <nic.kabs01@gmail.com>
2018-07-10 23:49:43 +0200

    administration side

--- src/app/user/make-payment/make-payment.component.ts
+++ src/app/user/make-payment/make-payment.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { takeWhile } from 'rxjs/operators';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-make-payment',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakePaymentComponent implements OnInit {
 
-  constructor() { }
-
+  user: firebase.User;
+  photoURL$: Observable<any>;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private storage: AngularFireStorage
+  ) { }
   ngOnInit() {
+    this.authService.currentUser$
+      .pipe(
+        takeWhile(user => !!user)
+        // tap(
+        //   user => (this.photoURL$ = this.storage.ref(user.uid).getDownloadURL())
+        // )
+      )
+      .subscribe(user => (this.user = user));
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/pages']);
+  }
+
+  isOnline(): boolean {
+    return true;
+  }
+
+  welcomeUser() {
+    return `You are logged in as, ${this.user.email}`;
   }
 
 }
