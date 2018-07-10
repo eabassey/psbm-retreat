c256939d36ec06b86ae748f420da8aedc1d112de
Enoch Abassey <eabassey@yahoo.com>
2018-07-10 22:16:13 +0200

    on payment setup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-  // encapsulation: ViewEncapsulation.Native
 })
 export class LoginComponent implements OnInit {
   email: string;
@@ -21,8 +17,6 @@ export class LoginComponent implements OnInit {
   constructor(
     public authService: AuthService,
     private router: Router,
-    private db: AngularFirestore,
-    private deviceService: DeviceDetectorService,
     private fb: FormBuilder
   ) {}
 
@@ -38,25 +32,6 @@ export class LoginComponent implements OnInit {
     this.authService
       .login(this.loginForm.value.email, this.loginForm.value.password)
       .then(value => {
-        // Store user login timestamp and env for audit
-        this.db
-          .collection(`/users`)
-          .doc(value.user.uid)
-          .collection('/logins')
-          .add({
-            timestamp: value.user.metadata.lastSignInTime,
-            browser: this.deviceService.browser,
-            browserVersion: this.deviceService.browser_version,
-            os: this.deviceService.os,
-            userAgent: this.deviceService.userAgent,
-            ua: this.deviceService.ua
-          });
-
-        // Get full user data
-        this.authService
-          .getFullUserData()
-          .subscribe(u => console.log('Checking login user: ', u));
-
         // On successful login redirect to user dashboard
         this.router.navigate(['/pages/home']);
         this.email = this.password = '';
--- src/app/user/make-payment/make-payment.component.ts
+++ src/app/user/make-payment/make-payment.component.ts
@@ -6,10 +6,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-payment.component.css']
 })
 export class MakePaymentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  // handleConferencePayment() {
+  //   // Render the PayPal button
+  //   paypal.Button.render(
+  //     {
+  //       // Set your environment
 
+  //       env: 'sandbox', // sandbox | production
+
+  //       // Specify the style of the button
+
+  //       style: {
+  //         layout: 'vertical', // horizontal | vertical
+  //         size: 'medium', // medium | large | responsive
+  //         shape: 'rect', // pill | rect
+  //         color: 'gold' // gold | blue | silver | black
+  //       },
+
+  //       // Specify allowed and disallowed funding sources
+  //       //
+  //       // Options:
+  //       // - paypal.FUNDING.CARD
+  //       // - paypal.FUNDING.CREDIT
+  //       // - paypal.FUNDING.ELV
+
+  //       funding: {
+  //         allowed: [paypal.FUNDING.CARD, paypal.FUNDING.CREDIT],
+  //         disallowed: []
+  //       },
+
+  //       // PayPal Client IDs - replace with your own
+  //       // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+
+  //       client: {
+  //         sandbox:
+  //           'ATS_Q78Kf58O8yffGYRr2jCRqpDm6R1H9wluCbgOeBOcsy12jj2NXARPqU8aWPNp_1ANsaRxNSfAc02s',
+  //         production:
+  //           ''
+  //       },
+  //       payment: (data, actions) => {
+  //         return actions.payment.create({
+  //           payment: {
+  //             transactions: [
+  //               {
+  //                 amount: {
+  //                   total: `5000`,
+  //                   currency: 'USD'
+  //                 }
+  //               }
+  //             ]
+  //           }
+  //         });
+  //       },
+
+  //       onAuthorize: (data, actions) => {
+  //         return actions.payment.execute().then(() => {
+  //           this.db.collection('/payments').add({
+  //             date: new Date(),
+  //             ...this.currentUserInfo,
+  //             ...data,
+  //             amount: 5000
+  //           });
+  //           window.alert('Payment Complete!');
+  //           this.router.navigate(['/user/payment-completed']);
+  //         });
+  //       }
+  //     },
+  //     '#paypal-button'
+  //   );
+  // }
 }
