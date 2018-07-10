97e415fa24ebf4878bc8036a1cfa0cd4bfbef3f7
Enoch Abassey <eabassey@yahoo.com>
2018-07-10 15:04:31 +0200

    removed unused login component from pages area

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,18 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages.routing';
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
-// import { AboutComponent } from './about/about.component';
-// import { ContactComponent } from './contact/contact.component';
-// import { InternationalVisitorsProgramComponent } from './international-visitors-program/international-visitors-program.component';
-// import { NgComponent } from './ng/ng.component';
-// import { PropheticSchoolMinistryComponent } from './prophetic-school-ministry/prophetic-school-ministry.component';
-// import { SheperdBushiriFoundationComponent } from './sheperd-bushiri-foundation/sheperd-bushiri-foundation.component';
-// import { ConferencesandseminarsComponent } from './conferencesandseminars/conferencesandseminars.component';
-// import { TelevisionMinistryComponent } from './television-ministry/television-ministry.component';
-// import { PartnershipComponent } from './partnership/partnership.component';
-import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
-// import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [PagesComponent, HomeComponent],
